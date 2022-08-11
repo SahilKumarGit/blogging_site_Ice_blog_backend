52eaa7a342cae1110a1f5c465bc9d4e1d6022c80
SahilKumarGit <email.sahil@yahoo.com>
2022-08-11 12:48:31 +0530

    p

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const route = require('./routers/router');
 const cors = require('cors')
 require('dotenv').config();
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 
 app.use(express.json({ limit: '50mb' }))
 app.use(multer().any())
--- src/config/aws.config.js
+++ src/config/aws.config.js
@@ -1,9 +1,10 @@
 const aws = require('aws-sdk');
+require('dotenv').config()
 
 
 aws.config.update({
-    accessKeyId: "AKIAQARKEUJJBLPSKF3L",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY_ID,
     region: "ap-south-1"
 })
 
